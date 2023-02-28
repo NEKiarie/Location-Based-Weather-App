8a62b99eb7a28cb159aa4b4467019b63fe47393f
+ <+>
2023-02-28 11:07:48 +0300

    :sparkles: Added Location View Layout

--- vue-project/src/components/BaseModal.vue
+++ vue-project/src/components/BaseModal.vue
@@ -1,20 +1,14 @@
 <template>
   <Teleport to="body">
     <Transition name="modal-outer">
-      <div
-        v-show="modalActive"
-        class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8"
-      >
+      <div v-show="modalActive"
+        class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8">
         <Transition name="modal-inner">
-          <div
-            v-if="modalActive"
-            class="p-4 bg-white self-start mt-32 max-w-screen-md rounded-3xl"
-          >
+          <div v-if="modalActive" class="p-4 bg-white self-start mt-32 max-w-screen-md rounded-3xl">
             <slot />
             <button
               class="text-weather-primary border hover:bg-weather-primary hover:text-white mt-8 bg-white py-2 px-6 rounded-full"
-              @click="$emit('close-modal')"
-            >
+              @click="$emit('close-modal')">
               Close
             </button>
           </div>
--- vue-project/src/components/SiteNavigation.vue
+++ vue-project/src/components/SiteNavigation.vue
@@ -1,8 +1,6 @@
 <template>
   <header class="sticky top-0 bg-weather-primary shadow-lg">
-    <nav
-      class="container flex flex-col sm:flex-row item-center gap-4 text-white py-6"
-    >
+    <nav class="container flex flex-col sm:flex-row item-center gap-4 text-white py-6">
       <RouterLink :to="{ name: 'home' }">
         <div class="flex items-center gap-3">
           <i class="fa-solid fa-sun text-2xl"></i>
@@ -11,13 +9,9 @@
       </RouterLink>
 
       <div class="flex gap-3 flex-1 justify-end">
-        <i
-          class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-150 cursor-pointer"
-          @click="toggleModal"
-        ></i>
-        <i
-          class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-150 cursor-pointer"
-        ></i>
+        <i class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-150 cursor-pointer"
+          @click="toggleModal"></i>
+        <i class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-150 cursor-pointer"></i>
       </div>
 
       <BaseModal :modalActive="modalActive" @close-modal="toggleModal">
@@ -60,6 +54,6 @@ import BaseModal from "./BaseModal.vue";
 
 const modalActive = ref(null);
 const toggleModal = () => {
-  modalActive.value = !modalActive.value; 
+  modalActive.value = !modalActive.value;
 };
 </script>
